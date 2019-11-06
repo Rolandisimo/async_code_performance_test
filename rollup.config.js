3f9c80a19ad01c0a9b99122f40b01ca5ae842a6c
Rolands Jegorovs <rolandjegorov@gmail.com>
2019-11-06 16:04:35 +0100

    Add terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
 
 export default [
   {
@@ -30,6 +31,7 @@ export default [
       }),
       resolve(),
       commonjs(),
+      terser(),
     ],
   },
 ];
